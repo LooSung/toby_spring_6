c1cda55651d3d53974f8988f1717b51b2b8e4967
emoldino-louis <woosung.choi@onepredict.com>
2024-07-16 13:45:42 +0900

    Session 2 3. Add Object Factory

--- src/main/java/tobyspring/hellospring/Client.java
+++ src/main/java/tobyspring/hellospring/Client.java
@@ -5,8 +5,10 @@ import java.math.BigDecimal;
 
 public class Client {
 	public static void main(String[] args) throws IOException {
-		PaymentService PaymentService = new PaymentService(new WebApiExRateProvider());
-		Payment payment = PaymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
+		ObjectFactory objectFactory = new ObjectFactory();
+		PaymentService paymentService = objectFactory.paymentService();
+
+		Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
 		System.out.println(payment);
 	}
 }
