5d25713e058ae5cdef7b8c280981551643beaa56
emoldino-louis <woosung.choi@onepredict.com>
2024-07-23 12:01:17 +0900

    Session 4 3. 디폴트 콜백과 템플릿 빈

--- src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
+++ src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
@@ -6,11 +6,15 @@ import tobyspring.hellospring.payment.ExRateProvider;
 import java.math.BigDecimal;
 
 public class WebApiExRateProvider implements ExRateProvider {
-	ApiTempalte apiTempalte = new ApiTempalte();
+	private final ApiTemplate apiTemplate;
+
+	public WebApiExRateProvider(ApiTemplate apiTemplate) {
+		this.apiTemplate = apiTemplate;
+	}
 
 	@Override public BigDecimal getExRate(String currency) {
 		String url = "https://open.er-api.com/v6/latest/" + currency;
 
-		return apiTempalte.getExRate(url, new HttpClientApiExecutor(), new ErApiExRateExtractor());
+		return apiTemplate.getForExRate(url);
 	}
 }
