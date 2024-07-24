e76ccc1c2d314f63a191bf7980eddd28581eb5a4
emoldino-louis <woosung.choi@onepredict.com>
2024-07-24 13:56:58 +0900

    Session 4 2. Repository & Exception part 1 : Unique Exception

--- src/main/java/tobyspring/hellospring/DataClient.java
+++ src/main/java/tobyspring/hellospring/DataClient.java
@@ -1,9 +1,8 @@
 package tobyspring.hellospring;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import tobyspring.hellospring.data.OrderRepository;
 import tobyspring.hellospring.order.Order;
 
 import java.math.BigDecimal;
@@ -11,15 +10,15 @@ import java.math.BigDecimal;
 public class DataClient {
 	public static void main(String[] args) {
 		BeanFactory beanFactory = new AnnotationConfigApplicationContext(DataConfig.class);
-		EntityManagerFactory emf = beanFactory.getBean(EntityManagerFactory.class);
-
-		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
+		OrderRepository orderRepository = beanFactory.getBean(OrderRepository.class);
 
 		Order order = new Order("100", BigDecimal.TEN);
-		em.persist(order);
+		orderRepository.save(order);
 
-		em.getTransaction().commit();
-		em.close();
+		Order order2 = new Order("100", BigDecimal.ONE);
+		orderRepository.save(order2);
+
+		System.out.println(order);
+
 	}
 }
