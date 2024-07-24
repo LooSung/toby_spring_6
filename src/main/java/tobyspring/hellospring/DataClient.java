540e3a9fd468a10b03e1a688cc96da41ecc40190
emoldino-louis <woosung.choi@onepredict.com>
2024-07-24 14:15:38 +0900

    Session 4 2. Repository & Exception part 2 (DataAccessException) : 기술에 영향을 받지 않은 예외처리

--- src/main/java/tobyspring/hellospring/DataClient.java
+++ src/main/java/tobyspring/hellospring/DataClient.java
@@ -2,6 +2,12 @@ package tobyspring.hellospring;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 import tobyspring.hellospring.data.OrderRepository;
 import tobyspring.hellospring.order.Order;
 
@@ -11,14 +17,20 @@ public class DataClient {
 	public static void main(String[] args) {
 		BeanFactory beanFactory = new AnnotationConfigApplicationContext(DataConfig.class);
 		OrderRepository orderRepository = beanFactory.getBean(OrderRepository.class);
+		JpaTransactionManager jtm = beanFactory.getBean(JpaTransactionManager.class);
 
-		Order order = new Order("100", BigDecimal.TEN);
-		orderRepository.save(order);
+		try {
+			new TransactionTemplate(jtm).execute(status -> {
+				Order order = new Order("100", BigDecimal.TEN);
+				orderRepository.save(order);
 
-		Order order2 = new Order("100", BigDecimal.ONE);
-		orderRepository.save(order2);
+				Order order2 = new Order("100", BigDecimal.ONE);
+				orderRepository.save(order2);
 
-		System.out.println(order);
-
+				return null;
+			});
+		} catch (DataIntegrityViolationException e) {
+			System.out.println("주민번호 중복 복구 작업");
+		}
 	}
 }
